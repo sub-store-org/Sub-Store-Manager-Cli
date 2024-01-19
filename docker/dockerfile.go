79ae1e52a2d6172138e5cf9120efb4cd2b1bd72e
DesnLee <jiakun.ui@gmail.com>
2024-01-20 00:51:31 +0800

    feat: use dist.zip instead of downloading the master when building the frontend image

--- docker/dockerfile.go
+++ docker/dockerfile.go
@@ -30,26 +30,15 @@ RUN apt-get update && \
     apt-get install -y curl unzip && \
     rm -rf /var/lib/apt/lists/* && \
     curl -LJO https://sub-store-org.github.io/resource/ssm/nginx.conf && \
-    curl -o master.zip -LJ https://github.com/sub-store-org/Sub-Store-Front-End/archive/refs/heads/master.zip && \
-    unzip master.zip
-
-FROM node:16-alpine AS builder
-
-WORKDIR /app
-COPY --from=downloader /app/Sub-Store-Front-End-master/package.json /app/Sub-Store-Front-End-master/pnpm-lock.yaml ./
-RUN npm install -g pnpm && pnpm i
-
-COPY --from=downloader /app/Sub-Store-Front-End-master/src ./src
-COPY --from=downloader /app/Sub-Store-Front-End-master/public ./public
-COPY --from=downloader /app/nginx.conf /app/Sub-Store-Front-End-master/.env /app/Sub-Store-Front-End-master/.env.production /app/Sub-Store-Front-End-master/index.html /app/Sub-Store-Front-End-master/tsconfig.json /app/Sub-Store-Front-End-master/tsconfig.node.json /app/Sub-Store-Front-End-master/vite.config.ts ./
-
-RUN pnpm build
+    curl -o dist.zip -LJ https://github.com/sub-store-org/Sub-Store-Front-End/releases/latest/download/dist.zip && \
+    unzip dist.zip
 
 FROM nginx:alpine AS runner
 
 WORKDIR /app
-COPY --from=builder /app/dist ./www
-COPY --from=builder /app/nginx.conf /etc/nginx/conf.d/default.conf
+
+COPY --from=downloader /app/dist ./www
+COPY --from=downloader /app/nginx.conf /etc/nginx/conf.d/default.conf
 
 EXPOSE 80
 
